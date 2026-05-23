8d4867dd912bbcf370cce2fd6ab2ea6e28d89399
Nico Krause <post@nicokrause.com>
2026-05-23 11:27:04 +0500

    chore: fix lint

--- js-peer/src/components/sponsor-relay-nav-button.tsx
+++ js-peer/src/components/sponsor-relay-nav-button.tsx
@@ -2,12 +2,9 @@ import dynamic from 'next/dynamic'
 
 import { useLibp2pContext } from '@/context/ctx'
 
-const SponsorRelayFab = dynamic(
-  () => import('@le-space/ui/react').then((mod) => mod.SponsorRelayFab),
-  {
-    ssr: false,
-  },
-)
+const SponsorRelayFab = dynamic(() => import('@le-space/ui/react').then((mod) => mod.SponsorRelayFab), {
+  ssr: false,
+})
 
 export default function SponsorRelayNavButton() {
   const { libp2p } = useLibp2pContext()
